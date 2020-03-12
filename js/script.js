289a5ea3d5ff414a21881ceddbbc0612773f73cd
Amy B <aback1227@gmail.com>
2020-03-12 00:21:41 -0400

    sticky navbar with bg color when scrolling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 window.onscroll = () => {stickyNav()};
 
 function stickyNav() {
-  let el = document.getElementById("about").offsetTop;
+  let el = document.getElementById("navbar").offsetTop;
   let nav = document.getElementById("navbar");
 
-  if (window.pageYOffset >= el) {
+  if (window.pageYOffset >= 20) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
